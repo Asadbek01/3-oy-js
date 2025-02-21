6f9741f01a28bdeed58774ea50b6ea339f1ea2cb
Asadbek <asadbek010azamjonov@gmail.com>
2025-02-21 09:50:14 +0500

    jsda o'zgaruvchilar

---
+++ index.js
@@ -0,0 +1,24 @@
+// O'zgaruvchi turlar
+//  var //es5  ----------------------   let va const // es6
+var c = 'Abdulloh';
+let surname = 'Odilov';
+// console.log(surname);
+const lorem = 'lorem';
+
+// O'zgaruvchi qiymatini o'zgartirish
+surname = 'Dilmurodov';
+console.log(surname);
+// lorem = 'Salimjonov'; // o'zgarmas chunki u const da chaqirilgan
+
+// var b = 999;
+// var l = 999;
+// console.log(b * l);
+// console.log(surname);
+// console.log(asdsadasd);
+
+// Kalit so'zlar
+// JavaScript -> dasturlash tili
+// Node -> JS ishga tushirish
+// V8 -> JS engine browserda ishlatish
+// Ulash -> <script src="file nomi"></script>
+// var, let const
