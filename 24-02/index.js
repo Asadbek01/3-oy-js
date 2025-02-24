2cda4011d34df120416141d1fe1f345bc6ceaf4e
Asadbek <asadbek010azamjonov@gmail.com>
2025-02-24 09:58:10 +0500

    yangi dars yakunlandi

---
+++ 24-02/index.js
@@ -0,0 +1,56 @@
+// Ma'lumot turlari
+
+// let va  const // es6
+
+// Strings:
+let ism = `212412`;
+
+//  Numbers:
+let num = 89;
+
+// Boolean:
+let isTrue = 'true';
+let isFalse = false;
+
+// Array object:
+let arr = [1, 2, 3, 4];
+// console.log(arr[3]);
+
+// Object:
+let compObj = {
+	xotira: '512GB',
+	nomi: 'LG',
+	yili: 2025,
+	yangimi: true,
+};
+
+// Date:
+let date = new Date();
+
+// console.log(typeof date);
+
+// Keywords:
+
+// data type:
+// string -> "", '', `` -> double quote, single quote, backtick
+// number -> 1,2,3,4,5
+// boolean -> true yoki false ni qiymat sifatida oladi.
+
+// array => [], elemntlarni index bo'ylab olamiz.
+// Object => {}, bunga biz property orqali bog'lanamiz.
+// eg: 1) compObj.nomi || 2) compObj["nomi"]
+// undefined -> topilmadi.
+// null -> null
+// Date:
+
+let bool = true; // boolean
+
+bool = ['ism', 'familya']; // array Object
+
+bool = {
+	isTrue: true,
+	nomi: 'boolean',
+}; // Object
+
+console.log(bool['isTrue']);
+console.log(bool.nomi);
