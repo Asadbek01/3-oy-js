bc79d0e8d0d0abdceeca3f8e040f56e51b9b9a02
Asadbek <asadbek010azamjonov@gmail.com>
2025-02-26 09:56:52 +0500

    #26-02 vazifalar yuklandi

---
+++ 26-02/new-lesson.js
@@ -0,0 +1,53 @@
+const person = {
+	name: 'LG',
+};
+
+// console.log(person['name']);
+
+let bool = true;
+
+bool = {
+	name: 'boolean',
+	age: '22',
+};
+
+bool = [2, 3, 4, true, 'hey'];
+
+const a = 5;
+const b = 8;
+const natija = a + b;
+// console.log(natija);
+
+// if (natija == 10) {
+// 	console.log('Ha teng');
+// } else {
+// 	console.log('Teng Emas');
+// }
+
+// natija == 11 ? console.log('teng') : console.log('teng emas'); // ternary
+
+// if (natija == 10) {
+// 	console.log('Ha teng');
+// } else if (natija == 11) {
+// 	console.log('Natija 11 ga teng');
+// } else {
+// 	console.log('Teng Emas');
+// }
+
+// keywords -> kalit so'zlar
+// ma'lumot turlari
+// if else  tushunchasi
+// ternary
+// o'zgaruvchi
+
+//----------------------------------------Vazifalar------------------------------------
+
+// 1)  1 ta firstName nomli o'zgaruvchi yarating. Va qiymatiga ismingizni bering uni consolega oling.
+
+// 2) 2ta o'zgaruvchi yarating nomlari num1 va num2 bo'lsin va yig'indisini alohida natija nomli o'zgaruvchiga oling.
+
+// 3) 2ta o'zgaruvchi kerak nomini o'zingiz bering. Va ularni yig'indisini yarmini topishingiz kk.
+
+// 4) Ikkinchi savoldagi natijani 10ga tenglang agar teng bo'lsa teng deb consele oling. Yoki teng emas.
+
+// 5 To'rtinchi savolni ternary bilan yeching
