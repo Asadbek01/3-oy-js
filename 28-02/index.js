856793122091fc0ea548dbb692a4a64d5875b88b
Asadbek <asadbek010azamjonov@gmail.com>
2025-02-28 10:00:12 +0500

    takrorlandi

---
+++ 28-02/index.js
@@ -0,0 +1,46 @@
+let object = {
+	name: 'Bilodin',
+	surname: 'Abdulhamidov',
+	age: [23, 28],
+};
+
+console.log(object.age[1]);
+
+let arr = [
+	'olma',
+	'banan',
+	'ananas',
+	' kiwi',
+	{
+		nomi: 'meva',
+		vatani: ['America', 'Uzbekistan'],
+	},
+];
+
+console.log(arr[4].vatani[1]);
+
+let pc = {
+	nomi: 'Mac',
+	monitor: 'Ryzen',
+	prot: ['Cori7', 'gforce'],
+};
+console.log(pc.prot[1]);
+
+let nomi = {
+	nom: 'Mustafo',
+	hobbies: ['swim', 'run'],
+	business: ['IT', 'Firma'],
+};
+
+console.log(nomi.business[0]);
+
+const obj = {
+	isMarried: false,
+	age: 23,
+};
+
+if (obj.isMarried) {
+	console.log('uylangan');
+} else {
+	console.log('Uylanmagan');
+}
