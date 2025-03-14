0298e522fae9926542f143dfd9f4afbdd200b62b
Asadbek <asadbek010azamjonov@gmail.com>
2025-03-14 09:59:07 +0500

    array methods and homeworks

---
+++ 14-03/index.js
@@ -0,0 +1,54 @@
+// data types ->
+
+// string, number, boolean, array, object, null and undefined, date
+
+// .length, .push(), .pop(), .shift(), slice(startIndex, endIndex)
+
+const str = 'Asadbek'; // harflarni katta qiberish
+
+const array = [
+	'olma',
+	'nok',
+	true,
+	999,
+	{ name: 'Asadbek' },
+	{},
+	true,
+	999,
+	'lorem',
+];
+
+// array.push();
+// array.pop();
+// array.pop();
+// array.pop();
+// array.shift();
+
+const kesilganArray = array.slice(7, array.length);
+
+// Keywords:
+// push() -> yangi elemnt qo'shadi
+
+// pop() -> oxirgi elementni o'chirib tashlaydi.
+
+// shift() -> birinchi elementni olib tashlaydi
+
+// slice(start, end) -> belgilangan index bo'ylab kesib oladi
+
+const elements = ['olma', 1212, true];
+
+const arrayOfObj = [{}, {}, {}];
+
+// Vazifalar - >
+
+// 1) Hamma ma'lumot turlarini alohida o'zgaruvchiga oling.
+
+// 2) ikkita o'zgaruvchi yaratamiz va qiymatiga number beramiz ularni yig'indisini alohida o'zgaruvchiga olamiz.
+
+// 3) Bo'sh bolgan arrayni bitta o'zgaruvchiga saqlaymiz.
+
+// 4) Bo'sh bo'lgan arrayni array metodi (.push()) 4 ta element bilan to'ldiring.
+
+// 5) To'ldirilgan elemntlarni oxirgisini console.log oling.  (.length bn)
+
+// 6) getArray nomli funksiya yarating va u o'zida yaratilgan array param sifatida olsin.
